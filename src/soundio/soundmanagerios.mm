b934c61ceb958d6ff5f799f0620103e5457c7bb9
Pe13 <paolo.peforni@gmail.com>
2026-07-23 00:54:17 +0200

    Got headphones (and other possible output) working

--- src/soundio/soundmanagerios.mm
+++ src/soundio/soundmanagerios.mm
@@ -6,23 +6,38 @@
 namespace mixxx {
 
 void initializeAVAudioSession() {
-    AVAudioSession* session = AVAudioSession.sharedInstance;
+    AVAudioSession* session = [AVAudioSession sharedInstance];
     AVAudioSessionCategory category = AVAudioSessionCategoryPlayback;
     AVAudioSessionMode mode = AVAudioSessionModeDefault;
-    AVAudioSessionCategoryOptions options =
-            AVAudioSessionCategoryOptionMixWithOthers;
 
     NSError* error = nil;
-    [session setCategory:category mode:mode options:options error:&error];
+
+    // Set category first so the session knows we are doing playback
+    [session setCategory:category mode:mode options:0 error:&error];
     if (error != nil) {
         qWarning() << "Could not initialize AVAudioSession:"
                    << error.localizedDescription;
+        return;
+    }
+
+    // --- DYNAMIC CHANNEL QUERY ---
+    // Ask the hardware how many channels it physically supports
+    NSInteger maxChannels = [session maximumOutputNumberOfChannels];
+    qInfo() << "Hardware supports up to" << maxChannels << "output channels.";
+
+    // Request the maximum available channels
+    [session setPreferredOutputNumberOfChannels:maxChannels error:&error];
+    if (error != nil) {
+        qWarning() << "Could not set preferred output channels:"
+                   << error.localizedDescription;
+        return;
     }
 
     [session setActive:true error:&error];
     if (error != nil) {
         qWarning() << "Could not activate AVAudioSession:"
                    << error.localizedDescription;
+        return;
     }
 }
 
