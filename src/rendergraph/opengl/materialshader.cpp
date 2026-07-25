f7c7164c991a7c48e6edace3645a9fa6e8a79839
Pe13 <paolo.peforni@gmail.com>
2026-07-25 15:32:33 +0200

    Fix waveform with hardware acceleration

--- src/rendergraph/opengl/materialshader.cpp
+++ src/rendergraph/opengl/materialshader.cpp
@@ -2,8 +2,15 @@
 
 #include <QFile>
 #ifdef USE_QSHADER_FOR_GL
+#if QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
 #include <rhi/qshader.h>
+#else
+#include <private/qshader_p.h>
 #endif
+#include <QOpenGLContext>
+#endif
+
+#include <optional>
 
 #include "../../util/assert.h"
 
@@ -15,15 +22,65 @@ QString resource(const QString& filename) {
     return QStringLiteral(":/shaders/rendergraph/%1.qsb").arg(filename);
 }
 
+std::optional<QShaderKey> searchGlslEsShader(const QList<QShaderKey>& keys) {
+    std::optional<QShaderKey> selectedKey;
+    for (const auto& key : keys) {
+        if (key.source() != QShader::GlslShader ||
+                !(key.sourceVersion().flags() & QShaderVersion::GlslEs)) {
+            continue;
+        }
+        // Prefer newer version, or just take any GlEsShader
+        if (!selectedKey ||
+                key.sourceVersion().version() >
+                        selectedKey.value().sourceVersion().version()) {
+            selectedKey = key;
+        }
+    }
+    return selectedKey;
+}
+
+std::optional<QShaderKey> searchGlslShader(const QList<QShaderKey>& keys) {
+    std::optional<QShaderKey> selectedKey;
+    for (const auto& key : keys) {
+        if (key.source() != QShader::GlslShader ||
+                (key.sourceVersion().flags() & QShaderVersion::GlslEs)) {
+            continue;
+        }
+        // Prefer version 120 or the first available
+        if (!selectedKey || key.sourceVersion().version() == 120) {
+            selectedKey = key;
+        }
+    }
+    return selectedKey;
+}
+
 QByteArray loadShaderCodeFromFile(const QString& path) {
     QFile file(path);
     if (!file.open(QIODeviceBase::ReadOnly)) {
         qWarning() << "Failed to open the shader file:" << path;
         return QByteArray();
     }
-    QShader qsbShader = QShader::fromSerialized(file.readAll());
-    QShaderKey key(QShader::GlslShader, 120);
-    return qsbShader.shader(key).shader();
+    const auto qsbShader = QShader::fromSerialized(file.readAll());
+    std::optional<QShaderKey> selectedKey;
+    const QList<QShaderKey> keys = qsbShader.availableShaders();
+
+    const QOpenGLContext* context = QOpenGLContext::currentContext();
+    if (context && context->isOpenGLES()) {
+        // First look for OpenGL ES shaders
+        selectedKey = searchGlslEsShader(keys);
+    }
+    if (!selectedKey) {
+        // If not ES, or if we couldn't find a GLES shader, look for desktop GLSL
+        selectedKey = searchGlslShader(keys);
+    }
+    if (!selectedKey && !keys.isEmpty()) {
+        selectedKey = keys.first();
+    }
+
+    if (selectedKey) {
+        return qsbShader.shader(selectedKey.value()).shader();
+    }
+    return QByteArray();
 }
 #else
 QString resource(const QString& filename) {
